a59bbd1d48b9585d0b217f23a49864f13349f9ec
hassandaru <hassandaru@gmail.com>
2022-08-31 18:31:43 +1000

    added score, comment and outcome label, also image

--- hangman/ViewController.swift
+++ hangman/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     var gameTitle: UILabel!
-    var attempts: UILabel!
+    var attempts: UILabel! //0/7
     var guessed: UILabel!
     var hintLabel: UILabel!
     var correctlyGuessedAlphabetsLabel: UILabel!
@@ -111,26 +111,54 @@ class ViewController: UIViewController {
         //image on the right
         let imgAndOtherLabels = UIView()
         imgAndOtherLabels.translatesAutoresizingMaskIntoConstraints = false
-        imgAndOtherLabels.backgroundColor = .systemCyan
+        imgAndOtherLabels.backgroundColor = .systemGray2
         view.addSubview(imgAndOtherLabels)
+        
+        attempts = UILabel()
+        attempts.translatesAutoresizingMaskIntoConstraints = false
+        attempts.textAlignment = .left
+        attempts.backgroundColor = .black
+        attempts.textColor = .white
+        attempts.text = "0/7"
+        attempts.font = UIFont.systemFont(ofSize: 44)
+        imgAndOtherLabels.addSubview(attempts)
+        
+        comment = UILabel()
+        comment.translatesAutoresizingMaskIntoConstraints = false
+        comment.textAlignment = .left
+        comment.backgroundColor = .systemGray
+        comment.text = "CORRECT !!!/ TRY AGAIN !!!"
+        comment.font = UIFont.systemFont(ofSize: 24)
+        imgAndOtherLabels.addSubview(comment)
+        
+        outcome = UILabel()
+        outcome.translatesAutoresizingMaskIntoConstraints = false
+        outcome.textAlignment = .left
+        outcome.backgroundColor = .systemGray
+        outcome.text = "OUTCOME"
+        outcome.font = UIFont.systemFont(ofSize: 24)
+        imgAndOtherLabels.addSubview(outcome)
         
         hangmanImage = UIImage()
         hangmanImage = UIImage(named: "img1.png")
+        
         hangmanImageView = UIImageView(image: hangmanImage)
-        imgAndOtherLabels.addSubview(hangmanImageView)
-        
-        
+        hangmanImageView.translatesAutoresizingMaskIntoConstraints = false
+//        hangmanImageView.sizeToFit()
+//        hangmanImageView.set= CGRect(x: 0, y: 0, width: 135, height: 175)
 
+        imgAndOtherLabels.addSubview(hangmanImageView)
+//        view.addSubview(hangmanImageView)
         
         //Setting up constraints for everything
         NSLayoutConstraint.activate([
             
             gameTitle.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
-            gameTitle.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
-            gameTitle.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 100),
-
+//            gameTitle.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
+//            gameTitle.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: 100),
+            gameTitle.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor),
 //            gameTitle.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor),
-            gameTitle.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.8, constant: -100),
+            gameTitle.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.5),
             gameTitle.heightAnchor.constraint(equalToConstant: 60),
         //guessAndHintView First
             guessAndHintView.topAnchor.constraint(equalTo: gameTitle.bottomAnchor, constant: 20),
@@ -158,11 +186,35 @@ class ViewController: UIViewController {
 //
 //            //now setting img and otherlabels
             imgAndOtherLabels.topAnchor.constraint(equalTo: guessAndHintView.bottomAnchor, constant: 10),
-            imgAndOtherLabels.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100),
+            imgAndOtherLabels.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
 //            imgAndOtherLabels.heightAnchor.constraint(equalToConstant: 150),
-            imgAndOtherLabels.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.8)
+            imgAndOtherLabels.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.9, constant: -100),
+            imgAndOtherLabels.heightAnchor.constraint(equalToConstant: 300),
 //
+            //attempts, comments, outcome, img
+            attempts.topAnchor.constraint(equalTo: imgAndOtherLabels.topAnchor, constant: 10),
+            attempts.leadingAnchor.constraint(equalTo: imgAndOtherLabels.leadingAnchor, constant: 10),
+            attempts.widthAnchor.constraint(equalTo: imgAndOtherLabels.widthAnchor, multiplier: 0.5),
+            attempts.heightAnchor.constraint(equalToConstant: 60),
 
+            comment.topAnchor.constraint(equalTo: attempts.bottomAnchor, constant: 10),
+            comment.leadingAnchor.constraint(equalTo: imgAndOtherLabels.leadingAnchor, constant: 10),
+            comment.widthAnchor.constraint(equalTo: attempts.widthAnchor),
+            comment.heightAnchor.constraint(equalToConstant: 60),
+
+            outcome.topAnchor.constraint(equalTo: comment.bottomAnchor, constant: 10),
+            outcome.leadingAnchor.constraint(equalTo: imgAndOtherLabels.leadingAnchor, constant: 10),
+            outcome.widthAnchor.constraint(equalTo: attempts.widthAnchor),
+            outcome.heightAnchor.constraint(equalToConstant: 60),
+//
+            hangmanImageView.topAnchor.constraint(equalTo: imgAndOtherLabels.topAnchor, constant: 10),
+            hangmanImageView.trailingAnchor.constraint(equalTo: imgAndOtherLabels.trailingAnchor, constant: -10),
+            hangmanImageView.widthAnchor.constraint(equalToConstant: 135),
+            hangmanImageView.heightAnchor.constraint(equalToConstant: 190),
+//
+//
+//
+           
             
             
 
